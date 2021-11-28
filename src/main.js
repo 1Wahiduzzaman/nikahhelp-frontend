0e066c60e8b60798769f2314b241f2535c172378
Khorram Khondkar <khorramk.kbsk@gmail.com>
2021-11-28 21:12:33 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ Vue.component('Layout', Layout);
 import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
 
-const socketConnection = SocketIO('wss://66.29.130.69:4000');
+const socketConnection = SocketIO('https://matrimonyassist.com/chat_nodejs/');
 
 Vue.use(new VueSocketIO({
     debug: true,
