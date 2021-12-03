f24b87b4ab85abc1393c3b5f1d36856974a8eea4
Samir <samircu12@gmail.com>
2021-12-03 11:51:35 +0600

    'form'

--- src/router/guard/guard.js
+++ src/router/guard/guard.js
@@ -7,7 +7,7 @@ export const InitRoute = (to, from, next) => {
     else if (user.is_verified == 0) {
         return next({ name: 'EmailVerification' });
     }
-    else if (user.account_type === 1 && user.data_input_status <= 4) {
+    else if (user.account_type === 1 && user.data_input_status <= 5) {
 
         return next({ name: 'CandidateRegistration' });
     }
