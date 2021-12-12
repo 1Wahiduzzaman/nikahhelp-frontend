563028816b2dac302cd87f96c1de9621a3daba54
allImran <badhan19@gmail.com>
2021-12-13 00:18:58 +0600

    layout and responsive fix

--- src/components/dashboard/layout/Sidebar.vue
+++ src/components/dashboard/layout/Sidebar.vue
@@ -1,131 +1,150 @@
 <template>
 	<div class="d-sidebar" style="padding-right: 15px">
-		<ul class="sidebar-list" v-show="!collapsed">
-			<li class="list-item">
-				<router-link to="/search"
-					><img
-						class="mr-3"
-						src="@/assets/icon/search-love-secondary.svg"
-						alt="icon"
-					/>Search Your Match
-				</router-link>
-			</li>
-			<li class="list-item">
-				<router-link to="/manageteam" class="d-flex align-items-center">
-					<img class="mr-3" src="@/assets/icon/group-fill-secondary.svg" alt="icon" />
-					<span class="mb-0">Manage Team</span>
-					<a-badge
-						class="ml-auto"
-						:number-style="{ backgroundColor: '#e42076'}"
-						count="100"
-					/>
-				</router-link>
-			</li>
-			<li class="list-item">
-				<router-link to="/connections" class="d-flex align-items-center">
-					<img class="mr-3" src="@/assets/icon/connection-secondary.svg" alt="icon" />
-					<span class="mb-0">Connection</span>
-					<a-badge
-						class="ml-auto"
-						:number-style="{ backgroundColor: '#e42076'}"
-						count="17"
-					/>
-				</router-link>
-			</li>
-			<li class="list-item">
-				<router-link to="/shortlist"
-					><img
-						class="mr-3"
-						src="@/assets/icon/star-fill-secondary.svg"
-						alt="icon"
-					/>Shortlist
-				</router-link>
-			</li>
-			<li class="list-item">
-				<router-link to="/notifications" class="d-flex align-items-center">
-					<img class="mr-3" src="@/assets/icon/bell-fill-secondary.svg" alt="icon" />
-					<span class="mb-0">Notification</span>
-					<a-badge
-						class="ml-auto"
-						:number-style="{ backgroundColor: '#e42076'}"
-						count="40"
-					/>
-				</router-link>
-			</li>
-			<li class="list-item">
-				<router-link to="/chat-window" class="d-flex align-items-center">
-					<img class="mr-3" src="@/assets/icon/chat-dots-fill-secondary.svg" alt="icon" />
-					<span class="mb-0">Chat</span>
-					<a-badge
-						class="ml-auto"
-						:number-style="{ backgroundColor: '#e42076'}"
-						count="10"
-					/>
-				</router-link>
-			</li>
-			<li class="list-item">
-				<router-link to="/blocklist"
-					><img
-						class="mr-3"
-						src="@/assets/icon/block-secondary.svg"
-						alt="icon"
-					/>Block List
-				</router-link>
-			</li>
-			<li class="list-item">
-				<router-link to="/profile"
-					><img
-						class="mr-3"
-						src="@/assets/icon/person-fill-secondary.svg"
-						alt="icon"
-					/>Profile
-				</router-link>
-			</li>
-			<li class="list-item">
+		<template v-if="path == 'AdvanceSearch'">
+			<div>
+				<SidebarHeader v-if="!collapsed"/>
+				<div class="mt-5">
+					<div class="select-box">
+						<select class="custom-select" v-model="maritalStatus">
+							<option value="">Select Maritial Status</option>
+							<option value="single">Single</option>
+							<option value="married">Married</option>
+							<option value="divorced">Divorced</option>
+						</select>
+					</div>
+					
+				</div>
+			
+			</div>
+		</template>
+		<template v-else>
+			<ul class="sidebar-list" v-show="!collapsed">
+				<li  class="list-item">
+					<router-link to="/search"
+						><img
+							class="mr-3"
+							src="@/assets/icon/search-love-secondary.svg"
+							alt="icon"
+						/>Search Your Match
+					</router-link>
+				</li>
+				<li class="list-item">
+					<router-link to="/manageteam" class="d-flex align-items-center">
+						<img class="mr-3" src="@/assets/icon/group-fill-secondary.svg" alt="icon" />
+						<span class="mb-0">Manage Team</span>
+						<a-badge
+							class="ml-auto"
+							:number-style="{ backgroundColor: '#e42076'}"
+							count="100"
+						/>
+					</router-link>
+				</li>
+				<li class="list-item">
+					<router-link to="/connections" class="d-flex align-items-center">
+						<img class="mr-3" src="@/assets/icon/connection-secondary.svg" alt="icon" />
+						<span class="mb-0">Connection</span>
+						<a-badge
+							class="ml-auto"
+							:number-style="{ backgroundColor: '#e42076'}"
+							count="17"
+						/>
+					</router-link>
+				</li>
+				<li class="list-item">
+					<router-link to="/shortlist"
+						><img
+							class="mr-3"
+							src="@/assets/icon/star-fill-secondary.svg"
+							alt="icon"
+						/>Shortlist
+					</router-link>
+				</li>
+				<li class="list-item">
+					<router-link to="/notifications" class="d-flex align-items-center">
+						<img class="mr-3" src="@/assets/icon/bell-fill-secondary.svg" alt="icon" />
+						<span class="mb-0">Notification</span>
+						<a-badge
+							class="ml-auto"
+							:number-style="{ backgroundColor: '#e42076'}"
+							count="40"
+						/>
+					</router-link>
+				</li>
+				<li class="list-item">
+					<router-link to="/chat-window" class="d-flex align-items-center">
+						<img class="mr-3" src="@/assets/icon/chat-dots-fill-secondary.svg" alt="icon" />
+						<span class="mb-0">Chat</span>
+						<a-badge
+							class="ml-auto"
+							:number-style="{ backgroundColor: '#e42076'}"
+							count="10"
+						/>
+					</router-link>
+				</li>
+				<li class="list-item">
+					<router-link to="/blocklist"
+						><img
+							class="mr-3"
+							src="@/assets/icon/block-secondary.svg"
+							alt="icon"
+						/>Block List
+					</router-link>
+				</li>
+				<li class="list-item">
+					<router-link to="/profile"
+						><img
+							class="mr-3"
+							src="@/assets/icon/person-fill-secondary.svg"
+							alt="icon"
+						/>Profile
+					</router-link>
+				</li>
+				<li class="list-item">
 
-				<router-link to="/subscription"
-					><img
-						class="mr-3"
-						src="@/assets/icon/subscription-secondary.svg"
-						alt="icon"
-					/>Subscription
-				</router-link>
-			</li>
-			<li class="list-item">
-				<a href="#"
-					><img
-						class="mr-3"
-						src="@/assets/icon/support-secondary.svg"
-						alt="icon"
-					/>Support</a
-				>
-			</li>
-			<li class="list-item">
-				<router-link to="/settings"
-					><img
-						class="mr-3"
-						src="@/assets/icon/gear-fill-secondary.svg"
-						alt="icon"
-					/>Setting
-				</router-link>
-			</li>
-			<li class="list-item">
-				<a @click.prevent="logout"
-					><img
-						class="mr-3"
-						src="@/assets/icon/logout.svg"
-						alt="icon"
-					/>Logout</a
-				>
-			</li>
-		</ul>
-		<ul class="links" v-show="!collapsed">
-			<li><a href="#">Help</a></li>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Safety & Guidance</a></li>
-			<li><a href="#">Terms & Conditation</a></li>
-			<li><a href="/privacy-policy">Privacy Policy & Cookie Policy</a></li>
-		</ul>
+					<router-link to="/subscription"
+						><img
+							class="mr-3"
+							src="@/assets/icon/subscription-secondary.svg"
+							alt="icon"
+						/>Subscription
+					</router-link>
+				</li>
+				<li class="list-item">
+					<a href="#"
+						><img
+							class="mr-3"
+							src="@/assets/icon/support-secondary.svg"
+							alt="icon"
+						/>Support</a
+					>
+				</li>
+				<li class="list-item">
+					<router-link to="/settings"
+						><img
+							class="mr-3"
+							src="@/assets/icon/gear-fill-secondary.svg"
+							alt="icon"
+						/>Setting
+					</router-link>
+				</li>
+				<li class="list-item">
+					<a @click.prevent="logout"
+						><img
+							class="mr-3"
+							src="@/assets/icon/logout.svg"
+							alt="icon"
+						/>Logout</a
+					>
+				</li>
+			</ul>
+			<ul class="links" v-show="!collapsed">
+				<li><a href="#">Help</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Safety & Guidance</a></li>
+				<li><a href="#">Terms & Conditation</a></li>
+				<li><a href="/privacy-policy">Privacy Policy & Cookie Policy</a></li>
+			</ul>
+		</template>
 		<div class="trigger" @click="$emit('collapseSideBar')">
 			<a-icon :type="collapsed ? 'caret-right' : 'caret-left'" style="font-size: 20px" />
 		</div>
@@ -133,9 +152,17 @@
 </template>
 
 <script>
+import SidebarHeader from '@/components/dashboard/layout/SidebarHeader'
 export default {
 	name: "Sidebar",
-	components: {},
+	components: {
+		SidebarHeader
+	},
+	computed: {
+		path() {
+			return this.$route.name
+		}
+	},
 	props: {
 		collapsed: Boolean
 	},
