c31045edd68ef2a048b7e98b3f35547f76c4e4ed
allImran <badhan19@gmail.com>
2021-12-21 10:36:37 +0600

    add component modify, card switching improved

--- src/components/add/addComponent.vue
+++ src/components/add/addComponent.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>add</div>
+    <v-card
+            style="height: calc(100vh - 140px); overflow-y: auto; position: fixed; right: 10px;"
+            class="mx-auto"
+            width="317"
+        >
+        <div style="height: 100%">
+        <div class="text--disabled">add</div>
+            <img class="mx-auto" style="height: calc(100% - 50px); width: 280px; display: block" src="https://dummyimage.com/340x1:3/" alt="">
+        </div>
+    </v-card>
 </template>
 
 <script>
