19e8c99d7d40cdc4cdafec6f634d24ad43e36ac9
Samir <samircu12@gmail.com>
2022-01-04 21:23:39 +0600

    'a'

--- src/views/auth/Home.vue
+++ src/views/auth/Home.vue
@@ -1,16 +1,24 @@
 <template>
-	<div>
-		<Header />
-		<Banner />
-		<div class="bg-home-page">
-			<RecentlyJoined />
-			<Feature />
-			<Conversation />
-			<FindPartner />
-			<Footer />
-		</div>
-		<Cookies />
-	</div>
+  <div>
+    <!-- <div class="loader" v-if="isLoading">
+      <v-skeleton-loader
+        v-bind="attrs"
+        type="list-item-avatar-three-line, image, article"
+      ></v-skeleton-loader>
+    </div>
+    <div v-else> -->
+    <Header />
+    <Banner />
+    <div class="bg-home-page">
+      <RecentlyJoined />
+      <Feature />
+      <Conversation />
+      <FindPartner />
+      <Footer />
+    </div>
+    <Cookies />
+  </div>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -24,23 +32,42 @@ import Footer from "@/components/auth/Footer.vue";
 import Cookies from "@/components/cookies/Cookies.vue";
 
 export default {
-	name: "Home",
-	components: {
-		Header,
-		Banner,
-		RecentlyJoined,
-		Feature,
-		Conversation,
-		FindPartner,
-		Footer,
-		Cookies,
-	}
+  name: "Home",
+  components: {
+    Header,
+    Banner,
+    RecentlyJoined,
+    Feature,
+    Conversation,
+    FindPartner,
+    Footer,
+    Cookies,
+  },
+  data() {
+    return {
+      isLoading: false,
+      attrs: {
+        class: "mb-6",
+        boilerplate: true,
+        elevation: 2,
+      },
+    };
+  },
+  mounted() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  },
 };
 </script>
 
 <style scoped>
-	.bg-home-page {
-		background: url("~@/assets/full.svg") no-repeat center center / cover;
-		background-color: #f5f5ff;
-	}
+.loader {
+  height: 100vh;
+}
+.bg-home-page {
+  background: url("~@/assets/full.svg") no-repeat center center / cover;
+  background-color: #f5f5ff;
+}
 </style>
