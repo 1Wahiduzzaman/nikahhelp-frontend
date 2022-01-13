44db7a523cb9dbdc1761713d325c609ae0197680
Mahmud Hasan Shakkhor <mahmudshakkhor@gmail.com>
2022-01-13 13:32:26 +0600

    MATS-212 #comment Support chat completed

--- src/components/team/InviteMember.vue
+++ src/components/team/InviteMember.vue
@@ -69,25 +69,27 @@
         <div class="mt-2" v-if="showUserBox">
 <!--          <h6 class="text-white fs-14">Attach a user to this invitation</h6>-->
           <a-input ref="userNameInput" class="mt-1" placeholder="Search email or user ID" v-model="user_email" @input="searchMember()" medium>
-            <a-icon slot="suffix" type="info-circle" style="color: rgba(0,0,0,.45)" />
+            <a-icon slot="suffix" type="loading" style="color: rgba(0,0,0,.45)" v-if="searchLoading" />
+            <a-icon slot="suffix" type="info-circle" style="color: rgba(0,0,0,.45)" v-if="!searchLoading" />
           </a-input>
         </div>
 <!--        <span class="text-white fs-12 fw-500 ml-2">Invited/Suggested/Searched user</span>-->
       </div>
       <div class="suggestion-box mt-4 mx-4 px-2" :class="{'details-suggestion-card': from === 'details-card'}">
         <div class="user d-flex position-relative" v-if="userObj && userObj.user">
-          <img src="https://picsum.photos/200" alt="avatar" class="user-avatar">
-          <div class="d-flex justify-content-between ml-2">
+          <img src="https://picsum.photos/200" alt="avatar" class="user-avatar" />
+          <div class="d-flex justify-content-between align-items-center ml-2">
             <div class="short-info">
               <h4 class="fs-14 text-white fw-700">{{ userObj.user && userObj.user.full_name ? userObj.user.full_name : 'N/A' }}</h4>
               <h4 class="fs-12 text-white fw-500 candidate-type">Profile type: {{ userObj.user ? profileType[userObj.user.account_type] : 'N/A' }}</h4>
             </div>
             <button class="btn btn-sent position-absolute text-white cursor-default" v-if="userObj.invitation_status == 2">Joined</button>
             <button class="btn btn-sent btn-outline-secondary position-absolute text-white cursor-default" v-if="userObj.invitation_status == 1">Sent</button>
-            <button class="btn btn-success position-absolute" :disabled="isSuccess"
-                    v-if="userObj.invitation_status == 0" @click="attachUser()">
-              {{ userObj && userObj.user && userObj.user.email == invitationObject.email ? 'Invited' : 'Invite' }}
-            </button>
+<!--            <button class="btn btn-success position-absolute" :disabled="isSuccess"-->
+<!--                    v-if="userObj.invitation_status == 0" @click="attachUser()">-->
+<!--              {{ userObj && userObj.user && userObj.user.email == invitationObject.email ? 'Invited' : 'Invite' }}-->
+<!--            </button>-->
+            <a-checkbox class="position-absolute btn-sent" @change="attachUser" v-model="attached" v-if="userObj.invitation_status == 0"></a-checkbox>
           </div>
         </div>
         <button class="btn invitation-link-btn btn-block btn-sm py-2 mt-2" @click="generateLink" v-if="showUserBox" :disabled="isLoading || isSuccess"><a-icon type="loading" v-if="isLoading" /> Generate Invitation Link</button>
@@ -118,6 +120,8 @@ export default {
       showUserBox: false,
       isLoading: false,
       isSuccess: false,
+      searchLoading: false,
+      attached: false,
       invitationObject: {
         role: undefined,
         add_as_a: undefined,
@@ -132,13 +136,20 @@ export default {
   },
   methods: {
     async searchMember() {
+      this.searchLoading = true;
       await ApiService.post(`/v1/user-info`, {
         email: this.user_email,
         team_id: this.team.id
       }).then(response => {
+        this.invitationObject.email = null;
+        this.attached = false;
         this.userObj = response.data.data;
+        this.searchLoading = false;
       }).catch(e => {
         this.userObj = {};
+        this.searchLoading = false;
+        this.invitationObject.email = null;
+        this.attached = false;
       });
     },
     inviteMember() {
@@ -152,7 +163,11 @@ export default {
       }
     },
     attachUser() {
-      this.invitationObject.email = this.userObj.user.email;
+      if(this.attached) {
+        this.invitationObject.email = this.userObj.user.email;
+      } else {
+        this.invitationObject.email = null;
+      }
     },
     removeAttachedUser() {
       this.invitationObject.email = null;
