1a0561b4f302562914067feaddf47906d311b13a
Mahmud Hasan Shakkhor <shakkhor@tikweb.dk>
2022-01-18 00:36:53 +0600

    Relationship in creation process

--- src/components/team/CreateTeamPage1.vue
+++ src/components/team/CreateTeamPage1.vue
@@ -304,7 +304,7 @@ export default {
         this.loading = true;
         this.in_progress = true;
         this.team.add_as_a = this.addAs;
-        this.team.relationship = this.selfRole.relationship;
+        this.team.relationship = this.addAs == 'Candidate' ? 'Candidate' : this.selfRole.relationship;
         let formData = new FormData();
         formData.append('logo', this.file);
         Object.keys(this.team).map(data =>{
