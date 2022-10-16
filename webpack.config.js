acb1809677542b62eb94bebcc3b65ca0765dca93
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2022-10-16 14:42:04 +0100

    optimise

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/',
-    filename: '[name].[contenthash].js',
+    filename: '[name].js',
   },
 
   optimisation: {
