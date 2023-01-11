5e25503cce8fd2810aa760f8cb0e667d1a34bc9a
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-01-11 19:04:00 +0600

    Add cookies-preference modal

--- src/components/cookies/Cookies.vue
+++ src/components/cookies/Cookies.vue
@@ -8,9 +8,22 @@
         centered
         @ok="() => (modal2Visible = false)"
       >
+        <div style="overflow-y:scroll; height: 500px; overflow-x:hidden">
+
+          <CookiesAgreement />
+        </div>
+      </a-modal>
+
+      <a-modal
+        v-model="preferenceModalVisible"
+        title="Cookies Preference"
+        width="575px"
+        centered
+        @ok="preferenceModalVisible = false"
+      >
 			<div style="overflow-y:scroll; height: 500px; overflow-x:hidden">
 
-        <CookiesAgreement />
+        <CookiesPreference />
 			</div>
       </a-modal>
     </div>
@@ -30,10 +43,12 @@
       <span aria-hidden="true">&times;</span>
       </button>
       
-           <div class="cookie-policy-popup">We use cookies on this website to distinguish you from other users. 
+           <div class="cookie-policy-popup">We use essential cookies to make our site work. With your consent, we may also use non-essential cookies to improve user experience and analyse website traffic.  By clicking “Accept, you consent to our use of cookies as described in our <span style="color: #1976d2; cursor: pointer;" @click="modal2Visible = true">Privacy and Cookie Policy.</span> You can change your cookie settings at any time by clicking  <button class="settingButton" @click="preferenceModalVisible = true">“Preferences.”</button></div>
+
+           <!-- <div class="cookie-policy-popup">We use cookies on this website to distinguish you from other users. 
              We use this data to enhance your experience and do not use it for targeted advertising by any third party organisations. 
              By continuing to use this website you consent to our use of cookies.
-             For more information please see our <button class="settingButton" @click="modal2Visible = true">cookies policy.</button> </div>
+             For more information please see our <button class="settingButton" @click="modal2Visible = true">cookies policy.</button> </div> -->
 
       <div class="buttons">
         <button type="submit" @click="handleOk" class="acceptButton">
@@ -49,15 +64,16 @@
 </template>
 <script>
   import CookiesAgreement from './CookiesAgreement';
+  import CookiesPreference from './CookiesPreference';
   import JwtService from '../../services/jwt.service';
 
   export default {
     name: 'Cookies',
-    components: { CookiesAgreement },
+    components: { CookiesAgreement, CookiesPreference },
     data() {
       return {
         modal2Visible: false,
-
+        preferenceModalVisible: false,
         adVisible: true,
       };
     },
