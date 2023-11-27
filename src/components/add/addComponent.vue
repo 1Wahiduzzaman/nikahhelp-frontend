0afd308b2ba866446eb702a3a0c2626dbb09925b
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-11-27 11:20:23 +0600

    Update ad component

--- src/components/add/addComponent.vue
+++ src/components/add/addComponent.vue
@@ -5,8 +5,15 @@
             width="317"
         >
         <div style="height: 100%">
-        <div class="text--disabled">add</div>
-            <img class="mx-auto" style="height: calc(100% - 50px); width: 280px; display: block" src="https://dummyimage.com/340x1:3/" alt="">
+            <!-- <img class="mx-auto" style="height: 100%; width:100%; display: block" src="https://dummyimage.com/340x1:3/" alt=""> -->
+            <div class="mx-auto d-flex justify-content-center align-items-center" style="height: 100%; width: 100%; background: #cccccc">
+                <div>
+
+                    <h4 class="text--disabled">Advertising area</h4>
+                    <h4 class="text--disabled text-center">317 X 800</h4>
+
+                </div>
+            </div>
         </div>
     </v-card>
 </template>
